17d224d1dcfb6edec2295a478294c3093701a9b8
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-09-22 06:08:35 +0530

    Update tgraph.py

--- tgraph.py
+++ tgraph.py
@@ -1,1 +1,11 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from telegraph import upload_file
 
+tgraph = Client(
+    "TELEGRAPH BOT",
+    bot_token = os.environ["BOT_TOKEN"],
+    api_id = int(os.environ["API_ID"]),
+    api_hash = os.environ["API_HASH"]
+)
