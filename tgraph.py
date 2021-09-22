320a401cb2fe319005866c7d74c170f0fe701195
MR-JINN-OF-TG <84127769+MR-JINN-OF-TG@users.noreply.github.com>
2021-09-22 06:17:31 +0530

    Update tgraph.py

--- tgraph.py
+++ tgraph.py
@@ -9,3 +9,10 @@ tgraph = Client(
     api_id = int(os.environ["API_ID"]),
     api_hash = os.environ["API_HASH"]
 )
+
+@tgraph.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text(
+        text=f"Hai AM a simple telegraph uploader bot I can upload images under 5mb on telegram.ph 😐,
+        disable_web_page_preview=True
+    )
